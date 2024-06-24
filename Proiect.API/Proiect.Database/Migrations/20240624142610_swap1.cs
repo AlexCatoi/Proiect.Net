108206e169ff93669cc4cb33ac047b79f0c929aa
MihaiEnacheGabriel <mihenache77@gmail.com>
2024-06-24 18:28:39 +0300

    Am scos ProjectsController - conflict accidental

---
+++ Proiect.API/Proiect.Database/Migrations/20240624142610_swap1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proiect.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class swap1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
